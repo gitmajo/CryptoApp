4be308c2223cb3b52f92b31255e993ac95cb47c6
PrzemekB <przemb@yahoo.com>
2019-02-10 22:52:12 +0100

    encrypt() fixed, decrypt() added

--- src/main.cpp
+++ src/main.cpp
@@ -7,45 +7,33 @@
 #include <numeric> //iota
 #include <random>
 
-void printText(std::vector<char>& raw, std::vector<char>& encrypted);
+void printText(const std::vector<char>& raw, const std::vector<char>& encrypted);
 void replaceSingleChar(std::vector<char>& input, char current, char target);
 void printMap(const std::map<char, char>& secretMap);
-void encryptText(std::vector<char>& encryptedText, const std::map<char, char>& secretMap);
+std::vector<char> encrypt(const std::vector<char>& plain, const std::map<char,char>& secretMap);
+std::vector<char> decrypt(const std::vector<char>& encrypted, const std::map<char,char>& secretMap);
 std::map<char, char> generateSecretMap();
+std::map<char, char> reverseMap(const std::map<char,char>& original);
 
 int main(){
     std::cout << "Hello world, cryptoApp!\n";
 
     const std::string inputText = "Stoi na stacji lokomotywa";
-    
-    const std::map<char, char> secretMap{
-        {'a', 'b'},
-        {'o', 'k'},
-        {'f', 'q'}
-    };
-    
-    std::vector<char> rawText(inputText.begin(), inputText.end());
-    std::vector<char> encryptedText(inputText.begin(), inputText.end());  
-    std::vector<char> encryptedTextX(inputText.begin(), inputText.end()); 
+    const std::vector<char> rawText(inputText.begin(), inputText.end());
 
-    printMap(secretMap);
-
-    printText(rawText, encryptedText);
-    
-    encryptText(encryptedText, secretMap);
-    printText(rawText, encryptedText);
- 
     auto privateMap = generateSecretMap();
-    printMap(privateMap);
-    
-    encryptText(encryptedTextX, privateMap);
-    printText(rawText, encryptedTextX);
+
+    std::vector<char> encrypted = encrypt(rawText, privateMap);
+    printText(rawText, encrypted);
  
+    std::vector<char> decrypted = decrypt(encrypted, privateMap);
+    printText(encrypted, decrypted); 
+
 
     return 0;
 }
 
-void printText(std::vector<char>& raw, std::vector<char>& encrypted){
+void printText(const std::vector<char>& raw, const std::vector<char>& encrypted){
     std::cout << "In:  ";
     std::copy(raw.begin(), raw.end(), std::ostream_iterator<char>(std::cout, ""));
     std::cout << "\n";    
@@ -54,21 +42,28 @@ void printText(std::vector<char>& raw, std::vector<char>& encrypted){
     std::cout << "\n";    
 }
 
-void replaceSingleChar(std::vector<char>& input, char current, char target){
-    std::replace(input.begin(), input.end(), current, target);
-}
 
-
-void printMap(const std::map<char, char>& secretMap){
+void printMap(const std::map<char,char>& secretMap){
     std::for_each(secretMap.begin(), secretMap.end(), [](const auto& p){
                 std::cout << p.first << ":" << p.second << "\n"; 
             });
 }
 
-void encryptText(std::vector<char>& encText, const std::map<char, char>& sMap){
-    std::for_each(sMap.begin(), sMap.end(), [&](auto& p){ 
-                    replaceSingleChar(encText, p.first, p.second); 
-                });
+std::vector<char> encrypt(const std::vector<char>& plain, const std::map<char,char>& sMap){
+    auto encrypted = plain;
+    char target;
+    std::replace_if(encrypted.begin(), encrypted.end(), 
+                    [&](char source){ return target = sMap.at(source);}, target);
+    return encrypted;
+}
+
+std::vector<char> decrypt(const std::vector<char>& encrypted, const std::map<char,char>& sMap){
+    auto decrypted = encrypted;
+    char target;
+    auto reversedMap = reverseMap(sMap);
+    std::replace_if(decrypted.begin(), decrypted.end(), 
+                    [&](char source){ return target = reversedMap.at(source);}, target);
+    return decrypted;
 }
 
 std::map<char, char> generateSecretMap(){
@@ -83,4 +78,11 @@ std::map<char, char> generateSecretMap(){
         tempMap[keys[i]] = values[i];
     
     return tempMap;
+}
+
+std::map<char, char> reverseMap(const std::map<char,char>& original){
+    std::map<char, char> reversed;
+    for(auto& p : original)
+        reversed[(p.second)] = (p.first);
+    return reversed;
 }
